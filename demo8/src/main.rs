87020651a7d91ddd31a974879b9d797e481ac4f0
dollarkiller <wangy@mvalley.io>
2020-07-28 09:59:54 +0800

    add demo8

--- demo8/src/main.rs
+++ demo8/src/main.rs
@@ -15,17 +15,32 @@ async fn main() -> io::Result<()>{
 async fn ttc() -> io::Result<()> {
     let mut listener = TcpStream::connect("0.0.0.0:8081").await?;
 
-    let (mut rd,mut wt) = io::split(listener);
+    // 读写分离
+    let (mut rd,mut wr) = io::split(listener);
+
+
+    // send
+    let write_task = tokio::spawn(async move {
+        wr.write_all(b"Hello\r\n").await?;
+        wr.write_all(b"World\r\n").await?;
+
+        Ok::<_,io::Error>(())
+    });
+
+    let mut buf = vec![0;128];
 
     loop {
-        let (mut socket,addr) = listener.accept().await?;
-        tokio::spawn(async move {
-            println!("addr: {}",addr.to_string());
-            fc(socket).await;
-        });   
+            let n = rd.read(&mut buf).await?;
+            if n == 0 { // n == 0 read end
+                break;
+            }
+
+            println!("GOT: {:?}",&buf[..n]);
     }
+
+    Ok(())
 }
 
 async fn fc(socket: TcpStream) {
-
+    
 }
